55a9bde7dc91d3203262c1b1dd32993ab4f9d41e
Mahnoor-13 <mahnoor.abdullah20@gmail.com>
2022-12-23 13:56:35 +0500

    ...

--- src/Components/InnerNav/InnerNavContents/ResponsiveNav.js
+++ src/Components/InnerNav/InnerNavContents/ResponsiveNav.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import personalLoans from "../../../assets/personalLoans.png";
 import showmore from "../../../assets/showmore.png";
+import LoansInformations from "../../LoansInformations/LoansInformations";
+import { useNavigate } from "react-router-dom";
+import BlueButton from "../../BlueButton/BlueButton";
 import "../style.css";
 
-const responsiveNav = () => {
+const ResponsiveNav = () => {
+  let navigate = useNavigate();
+  const [showDescription, setShowDescription] = useState(false)
+
   return (
     <div className="responsive-center-nav">
-      <div className="tabs-background">
-        <div className="icon-title">
-          <img src={personalLoans} alt="personalloans" />
-          <p className="loan-title-responsive">Personal Loans</p>
+      <div>
+        <div className="tabs-background" onClick={() => setShowDescription(!showDescription)}>
+          <div className="icon-title">
+            <img src={personalLoans} alt="personalloans" />
+            <p className="loan-title-responsive">Personal Loans</p>
+          </div>
+          <img src={showmore} alt="showmore" />
         </div>
-        <img src={showmore} alt="showmore" />
-      </div>
 
+         {showDescription ?  <div>
+            <LoansInformations
+  
+              heading="Find the right personal loan rate for you
+
+"
+              text="Need to consolidate debt or make a large purchase? We bring the lenders to you so you can shop and compare personal loan offers in minutes.
+
+"
+            />
+            <BlueButton
+              title="Compare Rates"
+              onClick={() => navigate("/Questions")}
+            />
+          </div> : null}
+      </div>
 
       <div className="tabs-background">
         <div className="icon-title">
@@ -23,8 +46,6 @@ const responsiveNav = () => {
         <img src={showmore} alt="show more" />
       </div>
 
-
-
       <div className="tabs-background">
         <div className="icon-title">
           <img src={personalLoans} alt="personalloans" />
@@ -33,9 +54,6 @@ const responsiveNav = () => {
         <img src={showmore} alt="show more" />
       </div>
 
-
-
-
       <div className="tabs-background">
         <div className="icon-title">
           <img src={personalLoans} alt="personalloans" />
@@ -43,11 +61,8 @@ const responsiveNav = () => {
         </div>
         <img src={showmore} alt="show more" />
       </div>
-
-
-
     </div>
   );
 };
 
-export default responsiveNav;
+export default ResponsiveNav;
